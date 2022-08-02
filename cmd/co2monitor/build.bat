b0e33ed8c500e37bfb4f5bfaa8e2f00260a4b15c
henkman <henkman@github.com>
2022-08-02 22:27:21 +0200

    use trimpath

--- cmd/co2monitor/build.bat
+++ cmd/co2monitor/build.bat
@@ -1,3 +1,3 @@
 rsrc -ico Papirus-Team-Papirus-Apps-Weather.ico -manifest co2monitor.exe.manifest -o rsrc.syso
 go get -v -d
-go build -tags walk_use_cgo -ldflags="-H windowsgui -s -w"
+go build -trimpath -tags walk_use_cgo -ldflags="-H windowsgui -s -w"
